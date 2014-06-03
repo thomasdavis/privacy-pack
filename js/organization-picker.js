6b82696e7dd4631a713b0d4b058a9f0ff68cac0f
Chris Antaki <ChrisAntaki@gmail.com>
2014-06-02 19:40:57 -0700

    Updated organizations

--- js/organization-picker.js
+++ js/organization-picker.js
@@ -1,53 +1,53 @@
-window.onload = function() {
+$(function() {
   // Choose a random organization.
   var organizations = [
   {
-      // American Civil Liberties Union
-      "disclaimer": "<a href='http://aclu.org' target='_blank'>ACLU</a> will contact you about future campaigns. <a href=https://www.aclu.org/american-civil-liberties-union-privacy-statement' target='_blank'>Privacy policy</a>",
-      "tag": "reset-the-net-aclu",
-      "weight": 5
+    // American Civil Liberties Union
+    "disclaimer": "<a href='http://aclu.org' target='_blank'>ACLU</a> will contact you about future campaigns. <a href='https://www.aclu.org/american-civil-liberties-union-privacy-statement' target='_blank'>Privacy policy</a>",
+    "tag": "reset-the-net-aclu",
+    "weight": 2
   },
   {
-      // OpenMedia
-      "disclaimer": "<a href=http://openmedia.org' target='_blank'>OpenMedia</a> will contact you about future campaigns. <a href='https://openmedia.ca/privacy' target='_blank'>Privacy policy</a>",
-      "tag": "reset-the-net-openmedia",
-      "weight": 5
+    // OpenMedia
+    "disclaimer": "<a href='http://openmedia.org' target='_blank'>OpenMedia</a> will contact you about future campaigns. <a href='https://openmedia.ca/privacy' target='_blank'>Privacy policy</a>",
+    "tag": "reset-the-net-openmedia",
+    "weight": 0
   },
   {
-      // Demand Progress
-      "disclaimer": "<a href='http://demandprogress.org/' target='_blank'>Demand Progress</a> will contact you about future campaigns. <a href='http://www.demandprogress.org/privacy/' target='_blank'>Privacy policy</a>",
-      "tag": "reset-the-net-demand-progress",
-      "weight": 5
+    // Demand Progress
+    "disclaimer": "<a href='http://demandprogress.org/' target='_blank'>Demand Progress</a> will contact you about future campaigns. <a href='http://www.demandprogress.org/privacy/' target='_blank'>Privacy policy</a>",
+    "tag": "reset-the-net-demand-progress",
+    "weight": 0
   },
   {
-      // Fight for the Future
-      "disclaimer": "<a href='http://www.fightforthefuture.org/'>Fight for the Future</a> and <a href='http://www.thecenterforrights.org/'>Center for Rights</a> will contact you about future campaigns. <a href='http://www.fightforthefuture.org/privacy/'>Privacy Policy</a>",
-      "tag": "reset-the-net",
-      "weight": 10
+    // Fight for the Future
+    "disclaimer": "<a href='http://www.fightforthefuture.org/'>Fight for the Future</a> and <a href='http://www.thecenterforrights.org/'>Center for Rights</a> will contact you about future campaigns. <a href='http://www.fightforthefuture.org/privacy/'>Privacy Policy</a>",
+    "tag": "reset-the-net",
+    "weight": 98
   },
   {
-      // Free Press
-      "disclaimer": "<a href='http://www.freepress.net/' target='_blank'>Free Press</a> will contact you about future campaigns. <a href='https://www.freepress.net/privacy-copyright' target='_blank'>Privacy policy</a>",
-      "tag": "reset-the-net-freepress",
-      "weight": 5
+    // Free Press
+    "disclaimer": "<a href='http://www.freepress.net/' target='_blank'>Free Press</a> will contact you about future campaigns. <a href='https://www.freepress.net/privacy-copyright' target='_blank'>Privacy policy</a>",
+    "tag": "reset-the-net-freepress",
+    "weight": 0
   }
   ];
-  
+
   function getScore(weight) {
-      return (100 + weight) * Math.random();
+    return weight * Math.random();
   }
-  
+
   function getOrganization() {
-      return organizations.sort(function(a, b) {
-          return getScore(b.weight) - getScore(a.weight);
-      })[0];
+    return organizations.sort(function(a, b) {
+      return getScore(b.weight) - getScore(a.weight);
+    })[0];
   }
-  
+
+  // Update!
   var organization = getOrganization();
   $('input[name=tag]').val(organization.tag);
   $('.disclaimer p').html(organization.disclaimer);
-  
-};
+});
 
 
 $(document).ready(function(){
